48a1bb156fa5780892303d150550057c87efcbce
Admin <admin@gold-bot.local>
2026-06-02 14:26:24 +0700

    Bump release version to v1.9.0

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-VERSION = "1.8.116"
+VERSION = "1.9.0"
 GITHUB_REPO = "hedracentral9999/Hedra-Studio"
 LICENSE_VERIFY_URL = "https://license.boxphonefarm.com.vn/v1/licenses/verify"
 COMMERCIAL_LICENSE_URL = "https://hedrastudio.app/license"
